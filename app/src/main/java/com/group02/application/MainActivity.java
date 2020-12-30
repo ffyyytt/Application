70f2f4b19347ca762ba8c5551aadf9bfeaa020b7
Duong Anh Kiet <18120046@student.hcmus.edu.vn>
2020-12-31 00:25:56 +0700

    add MainActivity

--- app/src/main/java/com/group02/application/MainActivity.java
+++ app/src/main/java/com/group02/application/MainActivity.java
@@ -1,14 +1,47 @@
 package com.group02.application;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private LinearLayout linearLayout;
+    private Button button_sign_up;
+    private Button button_sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        linearLayout = (LinearLayout) findViewById(R.id.main_layout);
+        button_sign_up = (Button) findViewById(R.id.main_button_sign_up);
+        button_sign_in = (Button) findViewById(R.id.main_button_sign_in);
+
+        button_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        button_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
