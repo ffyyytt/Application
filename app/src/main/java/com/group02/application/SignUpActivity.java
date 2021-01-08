c2efc9e6c78100259ac41207ed017275f7fa5143
Duong Anh Kiet <18120046@student.hcmus.edu.vn>
2021-01-08 23:11:00 +0700

    pass login_check len

--- app/src/main/java/com/group02/application/SignUpActivity.java
+++ app/src/main/java/com/group02/application/SignUpActivity.java
@@ -121,7 +121,7 @@ public class SignUpActivity extends AppCompatActivity {
                             params.put("name", str_name);
                             params.put("phone_no", str_phone);
                             params.put("email", str_email);
-                            params.put("password", edt_password.getText().toString());
+                            params.put("password", str_password);
                             params.put("gender", str_gender);
                             params.put("point", "0");
                             return params;
