94dcd00331b5f30f1839e5af35f7db4a40735ff5
Hào Trần Chí <tranchihaodl@gmail.com>
2021-01-08 08:23:15 +0700

    Update SERVER.java

--- app/src/main/java/com/group02/application/SERVER.java
+++ app/src/main/java/com/group02/application/SERVER.java
@@ -1,7 +1,11 @@
 package com.group02.application;
 
 public class SERVER {
+<<<<<<< Updated upstream
     public static String get_server() {
+=======
+    String get_server() {
+>>>>>>> Stashed changes
         return "http://192.168.100.7:8000/";
     }
 }
